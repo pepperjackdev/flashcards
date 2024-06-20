590b8f737d282d3953f89050ee9350aba4800075
Giacomo De Florio <giacomo.deflorio.dev@gmail.com>
2024-06-20 16:59:11 +0200

    Added a collection_frame view and edited the collections view to be more resposive.
    Added a collection_frame(.fxml) view and refactored the collections(.fxml) view to be more responsive
    
    Added a collection_frame view; modified the collections view to make it responsive

--- src/main/java/io/github/pepperjackdev/flashcards/Collections.java
+++ src/main/java/io/github/pepperjackdev/flashcards/Collections.java
@@ -1,14 +1,18 @@
 package io.github.pepperjackdev.flashcards;
 
 import javafx.fxml.FXML;
+import javafx.scene.Parent;
 import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class Collections {
     @FXML Button addCollection;
-    @FXML ScrollPane collectionsList;
+    @FXML VBox collectionsList;
 
     @FXML void initialize() {
-        
+        addCollection.setOnAction(e -> {
+            Parent newCollectionFrame = App.loadFXML("collections/collection_frame.fxml");
+            collectionsList.getChildren().add(newCollectionFrame);
+        });
     }
 }
